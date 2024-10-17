201c4c37e6cc7ea41dc20f0c26131e2b59b88898
Feldwor <BoQsc@users.noreply.github.com>
2024-10-17 23:18:13 +0300

    Update streaming - custom_template.py

--- streaming - custom_template.py
+++ streaming - custom_template.py
@@ -19,14 +19,9 @@ with requests.post(url, headers=headers, stream=True, json=data) as response:
                 line = line[len("data: "):]  # Remove the prefix
 
             try:
-                # Attempt to parse the line as JSON
                 json_line = json.loads(line)
-                # Extract and print the text from 'choices'
-                #print(json_line["content"])
                 choices = json_line.get("content", [])
-                #print(choices)
-                #print("new line")
-                #input()
+
 
                 if choices:  # Print only non-empty content
                     print(choices, end='', flush=True)
