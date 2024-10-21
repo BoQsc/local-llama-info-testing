599f2f2409a3ffe3ae6955e7bd2f368b6eacf599
Feldwor <BoQsc@users.noreply.github.com>
2024-10-21 13:40:46 +0300

    Update gui_titlebar.py

--- gui_titlebar.py
+++ gui_titlebar.py
@@ -9,23 +9,34 @@ window.overrideredirect(True)
 
 #______________Title_Bar___________________
 titlebar = tk.Frame(window)
-titlebar.label = tk.Label(titlebar, text=window.title())
 
-titlebar.button = tk.Button(titlebar, command=lambda: minimize())
-titlebar.button.config(bg="#2c2c2c", fg='white', activebackground="red", activeforeground="white")
-titlebar.button.config(padx=5, pady=2, bd=0, width=2, highlightthickness=0)
-titlebar.button.bind("<Enter>", lambda event: event.widget.config(bg="#777777", fg='black'))
-titlebar.button.bind("<Leave>", lambda event: event.widget.config(bg="#2c2c2c", fg='white'))
-titlebar.button.config(text='🗕', font="bold")
+#______________Title_Bar_Name___________________
+titlebar.name = tk.Label(titlebar, text=window.title())
+
+#______________Title_Bar_Buttons___________________
+titlebar.minimize_button = tk.Button(titlebar, command=lambda: window.iconify())
+titlebar.maximize_button = tk.Button(titlebar, command=lambda: window.state('zoomed') if window.state() != 'zoomed' else window.state('normal'))
+titlebar.exit_button = tk.Button(titlebar, command=window.destroy)
+
+#______________Button_Configurations___________________
+for button in (titlebar.minimize_button, titlebar.maximize_button, titlebar.exit_button):
+    button.config(bg="#2c2c2c", fg='white', activebackground="red", activeforeground="white",
+                  padx=5, pady=2, bd=0, width=2, highlightthickness=0)
+    button.bind("<Enter>", lambda event: event.widget.config(bg="#777777", fg='black'))
+    button.bind("<Leave>", lambda event: event.widget.config(bg="#2c2c2c", fg='white'))
+
+#______________Button_Text___________________
+titlebar.minimize_button.config(text='🗕', font="bold")
+titlebar.maximize_button.config(text='🗖', font="bold")
+titlebar.exit_button.config(text='🗙', font="bold")
 
 #_______________Layout_____________________
 titlebar.pack(fill=tk.X)  
-titlebar.label.pack()  
-titlebar.button.pack()  
+titlebar.name.pack(side='left', padx=5)  
+titlebar.exit_button.pack(side=tk.RIGHT)  
+titlebar.maximize_button.pack(side=tk.RIGHT)  
+titlebar.minimize_button.pack(side=tk.RIGHT)  
 
-#button handlers
-def minimize():
-    print("testz")
 
 
 window.mainloop()
