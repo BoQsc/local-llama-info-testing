3b86e21c8bc05f7a1acf074978d7d61cb51809a4
Feldwor <BoQsc@users.noreply.github.com>
2024-10-22 02:08:24 +0300

    Create start - Copy.cmd

---
+++ Research1/start - Copy.cmd
@@ -0,0 +1,4 @@
+
+
+llamafile --log-disable --flash-attn --gpu DISABLE --main-gpu 1 --mlock  --threads 4  --server -m "C:\Users\Windows10\AppData\Local\nomic.ai\GPT4All\TheBloke\Ana-v1-m7-GGUF\ana-v1-m7.Q2_K.gguf" --chat-template deepseek --nobrowser 
+pause
