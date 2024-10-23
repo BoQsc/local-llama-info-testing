6787c157387ee5799dd7fdd4ffe346e495b5a9f4
Feldwor <BoQsc@users.noreply.github.com>
2024-10-23 19:46:01 +0300

    update

---
+++ Research1/start_llamacpp.cmd
@@ -0,0 +1,3 @@
+@ECHO OFF
+"C:\Users\Windows10\Documents\quicktemp\llama-b3923-bin-win-cuda-cu11.7.1-x64\llama-server.exe" -m "C:\Users\Windows10\AppData\Local\nomic.ai\GPT4All\TheBloke\Ana-v1-m7-GGUF\ana-v1-m7.Q2_K.gguf"
+pause
