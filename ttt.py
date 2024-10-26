11b0cf30a79207c5dadac5f83ea41064d8e9bb8a
Feldwor <BoQsc@users.noreply.github.com>
2024-10-26 14:30:02 +0300

    update

--- ttt.py
+++ ttt.py
@@ -16,21 +16,10 @@ set_window_attribute(hwnd, rendering_policy, ct.byref(value), ct.sizeof(value))
 window.geometry("300x300")  
 window.configure(bg='#2c2c2c')
 
+def toggle_fullscreen(event):
+    window.attributes('-fullscreen', not window.attributes('-fullscreen'))
 
-
-def toggle_fullscreen(event=None):
-    is_fullscreen = window.attributes('-fullscreen')
-    window.attributes('-fullscreen', not is_fullscreen)
-
-
-
-# Bind the F11 key
 window.bind('<F11>', toggle_fullscreen)
-
-# Initial fullscreen setting
-window.attributes('-fullscreen', True)
-
-# Optional: to exit fullscreen mode with the Escape key
 window.bind('<Escape>', lambda event: window.attributes('-fullscreen', False))
 
 window.mainloop()
