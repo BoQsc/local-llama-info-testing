48153e64eef73d0533d1ff92e0f939bb6d222c3b
Feldwor <BoQsc@users.noreply.github.com>
2024-10-26 15:07:25 +0300

    Update ttt.py

--- ttt.py
+++ ttt.py
@@ -30,23 +30,30 @@ set_immersive_dark_mode()
 window.geometry("300x300")
 window.configure(bg='#2c2c2c')
 window.update()
-
 import time
 time.sleep(1)
 
 print("setting to fullscreen")
 window.attributes('-fullscreen', not window.attributes('-fullscreen'))
-window.geometry("300x300")
 window.update()
 time.sleep(1)
-
-
 
 print("back from fullscreen")
 window.attributes('-fullscreen', not window.attributes('-fullscreen'))
 set_immersive_dark_mode()
 window.update()
 window.geometry("300x301")
+
+print("setting to fullscreen")
+window.attributes('-fullscreen', not window.attributes('-fullscreen'))
+window.update()
+time.sleep(1)
+
+print("back from fullscreen")
+window.attributes('-fullscreen', not window.attributes('-fullscreen'))
+set_immersive_dark_mode()
+window.update()
+window.geometry("300x300")
 
 print("done")
 
