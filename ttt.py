ec039abbb09f4e5d8861f185b79330e489db78ad
Feldwor <BoQsc@users.noreply.github.com>
2024-10-26 14:54:33 +0300

    Update ttt.py

--- ttt.py
+++ ttt.py
@@ -1,22 +1,44 @@
 import tkinter as tk
 import ctypes as ct
 
+
+# Function to set immersive dark mode
+def set_immersive_dark_mode():
+    DWMWA_USE_IMMERSIVE_DARK_MODE = 20
+    set_window_attribute = ct.windll.dwmapi.DwmSetWindowAttribute
+    get_parent = ct.windll.user32.GetParent
+    hwnd = get_parent(window.winfo_id())
+    rendering_policy = DWMWA_USE_IMMERSIVE_DARK_MODE
+    value = ct.c_int(2)
+    set_window_attribute(hwnd, rendering_policy, ct.byref(value), ct.sizeof(value))
+
+
 window = tk.Tk()
-window.title("Example")
+window.title("Program")
 window.update()
 
-DWMWA_USE_IMMERSIVE_DARK_MODE = 20
-set_window_attribute = ct.windll.dwmapi.DwmSetWindowAttribute
-get_parent = ct.windll.user32.GetParent
-hwnd = get_parent(window.winfo_id())
-rendering_policy = DWMWA_USE_IMMERSIVE_DARK_MODE
-value = ct.c_int(2)
-set_window_attribute(hwnd, rendering_policy, ct.byref(value), ct.sizeof(value))
+window.bind('<F11>', lambda event: (
+    window.update(),
+    set_immersive_dark_mode(),  # Set dark mode again
+    window.geometry("500x400"),
+    window.attributes('-fullscreen', not window.attributes('-fullscreen')),
+    set_immersive_dark_mode()  # Set dark mode after fullscreen toggle
+))
+window.bind('<Escape>', lambda event: window.attributes('-fullscreen', False))
 
-window.geometry("300x300")  
+set_immersive_dark_mode()
+window.geometry("300x300")
 window.configure(bg='#2c2c2c')
 
-window.bind('<F11>',    lambda event: window.attributes('-fullscreen', not window.attributes('-fullscreen')))
-window.bind('<Escape>', lambda event: window.attributes('-fullscreen', False))
+import time
+time.sleep(2)
+window.attributes('-fullscreen', not window.attributes('-fullscreen'))
+window.geometry("300x300")
+time.sleep(4)
+window.attributes('-fullscreen', not window.attributes('-fullscreen'))
+window.geometry("300x300")
+time.sleep(4)
+set_immersive_dark_mode()
+print("done")
 
 window.mainloop()
