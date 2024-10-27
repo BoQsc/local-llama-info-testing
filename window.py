14566b646a54cdac396644002ad27cb45d77e5c8
Feldwor <BoQsc@users.noreply.github.com>
2024-10-27 12:32:02 +0200

    update

--- window.py
+++ window.py
@@ -2,8 +2,10 @@ import tkinter as tk
 
 window = tk.Tk()
 window.title("Example")
-window.geometry("300x300")
+window.geometry("300x300")  # Width x Height
 window.overrideredirect(True)
 window.configure(bg='#2c2c2c')
+
+window.eval('tk::PlaceWindow %s center' % window.winfo_toplevel())
 
 window.mainloop()
