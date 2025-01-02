df525e66cde61f874e85c39994e7a76927368214
Feldwor <BoQsc@users.noreply.github.com>
2025-01-02 19:56:57 +0200

    Update simple.py

--- 2025/simple.py
+++ 2025/simple.py
@@ -1,27 +1,16 @@
 import tkinter
-
+# Containter, left aligned button, left aligned text area. As the text area grows left aligned button should center in the container vertically.
 window = tkinter.Tk()
 
-# Function to update text area height
-def update_text_area_height(event):
-    lines = text_area.get("1.0", tkinter.END).count("\n")
-    print(lines, text_area.winfo_height())
 
-    text_area.config(height=lines)
-
-# Frame setup
 frame = tkinter.Frame(borderwidth=2, relief="groove", bg="gray")
 frame.pack()
 
-# Button setup
 button = tkinter.Button(frame, text="User")
-button.pack(side=tkinter.LEFT, anchor=tkinter.N)
+button.pack(side=tkinter.LEFT)
 
-# Text area setup
-text_area = tkinter.Text(frame, width=40, height=1)
-text_area.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+text_area = tkinter.Text(frame, wrap=tkinter.WORD, width=40, height=5)
+text_area.pack(side=tkinter.RIGHT)
 
-# Bind KeyRelease event for real-time updates
-text_area.bind("<KeyPress>", update_text_area_height)
 
 window.mainloop()
