fdb245c0f5c52e211e83ca2f800fb350083d2377
Feldwor <BoQsc@users.noreply.github.com>
2025-01-03 17:01:19 +0200

    Update simple.py

--- 2025/simple.py
+++ 2025/simple.py
@@ -11,7 +11,7 @@ def create_auto_height_text(parent, width=40):
 if __name__ == "__main__":
     root = tk.Tk()
     frame = tk.Frame(root, bd=2, relief="groove", bg="gray")
-    frame.pack(padx=10, pady=10, expand=True, fill='both')
+    frame.pack(padx=10, pady=10, expand=True, )
     tk.Button(frame, text="User").pack(side=tk.LEFT)
     create_auto_height_text(frame).pack(side=tk.RIGHT, expand=True, fill='both')
     root.mainloop()
